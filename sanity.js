9d573f8d3064f3049ef77cb19c847313aeb57521
ashankritwik07 <110737148+ashankritwik07@users.noreply.github.com>
2024-11-20 12:44:44 +0530

    Add files via upload

---
+++ sanity.js
@@ -0,0 +1,5 @@
+const projectId = "7wk87vgj";
+const dataSet = "production";
+const apiUrl = `https://${projectId}.api.sanity.io/v2023-10-24/data/query/${dataSet}`;
+const token =
+  "skwx0oniT8Bazhv7ovdmUvb1wDjWjvQc9nShrDvG1CmGLd7fTL1Jt8UCet6oLseUB5QoqQ1Gza9r0uC7riqOHTbpNi4vSs6p0QAUX1MA9owAY6o3XtrOE0YbrMYYudWCwZKZAx31UWF1lNzXfvwSi46HaPLDfugOfMZ9PcGOIcMWnTaVPjG2";
