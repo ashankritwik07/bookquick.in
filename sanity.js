abd1ef5c6be15e4ccf71479c944cb36ce1afa3ff
ashankritwik07 <110737148+ashankritwik07@users.noreply.github.com>
2025-02-18 17:19:55 +0530

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -2,4 +2,4 @@ const projectId = "7wk87vgj";
 const dataSet = "production";
 const apiUrl = `https://${projectId}.api.sanity.io/v2023-10-24/data/query/${dataSet}`;
 const token =
-  "skwx0oniT8Bazhv7ovdmUvb1wDjWjvQc9nShrDvG1CmGLd7fTL1Jt8UCet6oLseUB5QoqQ1Gza9r0uC7riqOHTbpNi4vSs6p0QAUX1MA9owAY6o3XtrOE0YbrMYYudWCwZKZAx31UWF1lNzXfvwSi46HaPLDfugOfMZ9PcGOIcMWnTaVPjG2";
+  "skC2yjlPjxogTBwMZJgFZSgKzMaAxoNIHxfwKGipPvgW8N30xfKIFFbvOQEaJjghCysCe7AlnpVOGyWhKEV9VOPhGC2ohUbkRwTKchW5kVkM8XRuaiqkRXIvDh6BEKsQynfT89hht3XDl5AEVxvPnY0U51Tyi5kZzh8lGg5N5lFJAJxfe4lG";
